438e4daeb086ad063ae1a30984dddc302be3c90a
Ellenrud84 <ellenrud84@gmail.com>
2020-07-25 21:35:39 -0500

    created mediaquery for sidebar to go away on small screens.

--- WebVisualizations/app.js
+++ WebVisualizations/app.js
@@ -5,3 +5,13 @@ $(document).ready(function () {
     });
 
 });
+
+// function openNav(){
+//     document.getElementById("mySidebar").style.width = "250px";
+//     document.getElementById("mainSidebar").style.marginRight = "250px";
+//   }
+
+// function closeNav() {
+//     document.getElementById("mySidebar").style.width = "0";
+//     document.getElementById("mainSidebar").style.marginRight = "0";
+//   }
